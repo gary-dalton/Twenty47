3ea8679db3f419575f976a21799b18bfa6fa43ff
gary-dalton <gary@ggis.biz>
2014-05-10 10:28:32 -0500

    Create a common config

--- twenty47/config.py
+++ twenty47/config.py
@@ -1,21 +1,21 @@
 CSRF_ENABLED = True
-SECRET_KEY = 'udtu$%gutDu@*IthCaydBip8'
-DEBUG=True
-MONGODB_SETTINGS = {'DB': 'twenty47', "USERNAME": '247', 'PASSWORD':'twenty47'}
+SECRET_KEY = 'thesecretkey'
+DEBUG=False
+MONGODB_SETTINGS = {'DB': 'db_name', "USERNAME": 'db_user', 'PASSWORD':'db_pwd'}
 
 SECURITY_CONFIRMABLE = True
 SECURITY_REGISTERABLE = True
 SECURITY_RECOVERABLE = True
 SECURITY_TRACKABLE = True
 SECURITY_CHANGEABLE = True
-SECURITY_EMAIL_SENDER = 'dispatch_report@npexchange.org'
+SECURITY_EMAIL_SENDER = 'email_sender@yourbiz.com'
 
 MAIL_USE_SSL = True
-MAIL_SERVER = 'email-smtp.us-east-1.amazonaws.com'
+MAIL_SERVER = 'SMTP Server'
 MAIL_PORT = 465
-MAIL_PASSWORD = 'AsqSErTS/FSi6UO7xLDcJvIYV3G10O0qWSB89x36tHgp'
-MAIL_USERNAME = 'AKIAINXNANXPCYMFDQPA'
-MAIL_DEBUG = True
+MAIL_PASSWORD = 'smtp email pwd'
+MAIL_USERNAME = 'smtp email user'
+MAIL_DEBUG = False
 
 DISPATCH_ERROR_GENERAL = "Encountered a problem. Please try again. If this error persists, please contact your administrator."
 DISPATCH_ERROR_REMOTEADMIN = "Remote administration failed. Please login to complete your request."
